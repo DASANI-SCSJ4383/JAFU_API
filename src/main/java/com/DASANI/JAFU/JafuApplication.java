13137c25bcb35c3d8ccefbeb5a601bb84648ed82
Muhammad Nazirul Asyraaf Halid <mn.asyraaf@graduate.utm.my>
2022-05-30 17:35:29 +0800

    initial commit

---
+++ src/main/java/com/DASANI/JAFU/JafuApplication.java
@@ -0,0 +1,13 @@
+package com.DASANI.JAFU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JafuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JafuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DASANI/JAFU/JafuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DASANI.JAFU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JafuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
